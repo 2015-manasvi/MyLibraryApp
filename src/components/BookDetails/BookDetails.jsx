0976fc991db64d26393d091e87e54a3d70d6259b
shanmugamsivasankari <ssivasankari145@gmail.com>
2023-04-26 14:45:23 +0800

    MyLibraryApp

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./BookDetails.css";
 import { useNavigate } from "react-router-dom";
--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../../context.";
 import Book from "../BookList/Book";
-import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./BookList.css";
 
